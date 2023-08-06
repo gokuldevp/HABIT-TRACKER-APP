a30b1d7a83cdf2f5e68914221e0ebbabc4dd508a
Gokul Dev P <gokuldev.p123@gmail.com>
2023-08-06 19:28:53 +0530

    Update comments

--- controllers/details_controller.js
+++ controllers/details_controller.js
@@ -1,89 +1,142 @@
-// import the habit(db) from the models folder
+// Import required models and libraries
 const Habit = require('../models/habits');
 const HabitStatus = require('../models/habitStatus');
 
+// An array representing the days of the week
 const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
+// Controller to render the details view for a specific day
 module.exports.detailsView = async (req, res) => {
-    const navlinks = [{itemName:'Add Task',link:'/details/add-habit'},{itemName:'Weekly View',link:'/details/weekly-view'}]
-    const date = req.params['dayName'].slice(1);
-    
-    const habits = await Habit.find({});
-    const habitStatus = await HabitStatus.find({date})
+    // Define navigation links
+    const navlinks = [{ itemName: 'Add Task', link: '/details/add-habit' }, { itemName: 'Weekly View', link: '/details/weekly-view' }];
 
+    // Extract the date from the URL parameter (dayName)
+    const date = req.params['dayName'].slice(1);
+
+    // Fetch all habits and habit statuses for the given date
+    const habits = await Habit.find({});
+    const habitStatus = await HabitStatus.find({ date });
+
+    // Create a variables object to pass data to the view
     let variables = {
         title: date + " Habits",
         navLinks: navlinks,
         Habit: habits,
         habitStatus: habitStatus,
-    }
+    };
+
+    // Render the 'details' view with the data
     return res.render('details', variables);
 };
 
+// Controller to render the add habit view
 module.exports.addHabit = async (req, res) => {
-    const navlinks = [{itemName:'Weekly View',link:'/details/weekly-view'}]
+    // Define navigation links
+    const navlinks = [{ itemName: 'Weekly View', link: '/details/weekly-view' }];
+
+    // Create a variables object to pass data to the view
     let variables = {
-        title: "Add Habbit",
+        title: "Add Habit",
         navLinks: navlinks
-    }
+    };
+
+    // Render the 'addHabit' view with the data
     return res.render('addHabit', variables);
 };
 
+// Controller to render the weekly view
 module.exports.weeklyView = async (req, res) => {
-    const navlinks = [{itemName:'Add Task',link:'/details/add-habit'},]
+    // Define navigation links
+    const navlinks = [{ itemName: 'Add Task', link: '/details/add-habit' }];
+
+    // Count the total number of habits in the database
+    const totalhabits = await Habit.countDocuments();
+
+    // Array to store the days where all habits are marked as 'Done'
+    let doneWeeks = [];
+
+    // Iterate over the days of the week and check if all habits are done for each day
+    for (const day of daysOfWeek) {
+        let count = 0;
+        const habitlist = await HabitStatus.find({ date: day });
+        habitlist.forEach((habit) => {
+            if (habit.status === 'Done') {
+                count++;
+            }
+        });
+
+        if (count == totalhabits) {
+            doneWeeks.push(day);
+        }
+    }
+
+    // Create a variables object to pass data to the view
     let variables = {
         title: "Weekly View",
         navLinks: navlinks,
-        daysOfWeek: daysOfWeek
-    }
+        daysOfWeek: daysOfWeek,
+        done: doneWeeks
+    };
+
+    // Render the 'week' view with the data
     return res.render('week', variables);
 };
 
+// Controller to create a new habit
 module.exports.createHabit = async (req, res) => {
+    // Check if the habit with the given name already exists
     Habit.findOne({ name: req.body.name })
-    .exec()
-    .then((habit) => {
-        if (!habit) {
-
-            Habit.create(req.body)
-                .then((habit) => {
-                    
-
-                    daysOfWeek.forEach((day) => {
-                    HabitStatus.create({
-                        name: habit.name,
-                        time: habit.time,
-                        habit: habit._id,
-                        date: day,
-                        status: 'None',
-                      })
-                        .then((habitStatus) => {
-                        //   console.log(`HabitStatus created for ${day}:`, habitStatus);
-                        })
-                        .catch((error) => {
-                          console.error('Error creating HabitStatus:', error);
+        .exec()
+        .then((habit) => {
+            if (!habit) {
+                // If habit does not exist, create it
+                Habit.create(req.body)
+                    .then((habit) => {
+                        // For each day of the week, create a new HabitStatus entry with the habit details
+                        daysOfWeek.forEach((day) => {
+                            HabitStatus.create({
+                                name: habit.name,
+                                time: habit.time,
+                                habit: habit._id,
+                                date: day,
+                                status: 'None',
+                            })
+                                .then((habitStatus) => {
+                                    // Successfully created HabitStatus entry
+                                })
+                                .catch((error) => {
+                                    console.error('Error creating HabitStatus:', error);
+                                });
                         });
+                        // Redirect to the weekly view after successful creation
+                        return res.redirect('/details/weekly-view');
+                    })
+                    .catch((err) => {
+                        console.log("Error while creating details!");
+                        return;
                     });
-                    return res.redirect('/details/weekly-view');
-                })
-                .catch((err) => {
-                    console.log("Error while creating details!");
-                    return;
-                });
 
-        } else {
-            console.log("Habbit already exists");
-            return res.redirect('/details/weekly-view');
-        }
-    })
-    .catch((err) => {
-        console.log("Error in finding Habit!");
-        return;
-    });
+            } else {
+                // Habit already exists, redirect to the weekly view
+                console.log("Habit already exists");
+                return res.redirect('/details/weekly-view');
+            }
+        })
+        .catch((err) => {
+            console.log("Error in finding Habit!");
+            return;
+        });
 };
 
+// Controller to update the status of a habit
 module.exports.updateStatus = async (req, res) => {
+    // Extract the habitId and status from the request body
     const habitId = req.body.habitId;
-    await HabitStatus.findByIdAndUpdate({ _id: habitId}, {status: req.body.status});
+    const status = req.body.status;
+
+    // Find the HabitStatus document by its ID and update the status field
+    await HabitStatus.findByIdAndUpdate({ _id: habitId }, { status: status });
+
+    // Redirect back to the previous page
     res.redirect('back');
-}
+};
